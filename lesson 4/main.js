87c8f4a22994fb7a45f524b4957894a88553b1aa
zafarbek2005 <153996622+zafarbek2005@users.noreply.github.com>
2024-01-31 23:22:18 +0500

    .

---
+++ lesson 4/main.js
@@ -0,0 +1,65 @@
+// N 1
+// a=prompt("a ni kriting")
+// b=prompt("b ni kriting")
+// for (var i = a; i <= b; i++) {
+//     if (parseInt(a)==a &&parseInt(b)==b)
+//     console.log(i);
+//   }
+  
+// N 2
+// a=+prompt("a ni kriting")
+// b=+prompt("b ni kriting")
+// for (var i = 2; i < b; i++) {
+//     if (parseInt(a)==a &&parseInt(b)==b)
+//     console.log(i);
+//   }
+
+
+// N 3
+// a=prompt(" 1 kg kanfet narxini kriting")
+// for (var i=1; i<10; i+=1){
+//     console.log(i +  i*a,"som"   );
+// }
+
+
+// N 4
+// a=prompt("1 kg kanfet narxini kriting")
+// for (var i=1.2; i<=11; i+=1.2){
+//     console.log(i +  i*a,"som"   );
+
+//     100
+// }
+
+// N 5
+// a=+prompt("a ni kriting")
+// b=+prompt("b ni kriting")
+// sum=0
+// for (var i = a; i <= b; i++) {
+//     if (parseInt(a)==a &&parseInt(b)==b)
+//     sum+=i
+//   }
+//   console.log(sum);
+// N 6
+
+// a=+prompt("a ni kriting")
+// b=+prompt("b ni kriting")
+// sum=1
+// for (var i = a; i <= b; i++) {
+//     if (parseInt(a)==a &&parseInt(b)==b)
+//     sum*=i
+   
+//   }
+// console.log(sum);
+// N 7
+
+// a=+prompt("a ni kriting")
+// b=+prompt("b ni kriting")
+// sum=1
+// for (var i = a; i <= b; i++) {
+//     if (parseInt(a)==a &&parseInt(b)==b)
+//     sum+=i**2
+//   }
+//   console.log(sum);
+
+// N 8
+
