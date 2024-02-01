d1c45e96cb6d4277957d7b8a6011b8b867416d3e
zafarbek2005 <153996622+zafarbek2005@users.noreply.github.com>
2024-02-01 09:31:43 +0500

    .

--- lesson 4/main.js
+++ lesson 4/main.js
@@ -61,5 +61,131 @@
 //   }
 //   console.log(sum);
 
-// N 8
+// N 20
+// x = parseInt(prompt())
+// nums = ""
 
+// for (let i = 1; i <=x; i++) {
+//     nums += String(i) + " "
+//     console.log(nums);
+// }
+
+// N 11
+
+// a=+prompt("a ni kriting")
+// n=+prompt("darajani kriting")
+// for (var i=1;i<=a;i++){
+//     console.log(i**2);
+// }
+
+
+// N 12
+
+// a=+prompt("a ni kriting")
+// n=+prompt("darajani kriting")
+// for (var i=1;i<=a;i++){
+//     if(n>0){
+//         console.log(i**2);
+//     }
+
+
+// N 13
+
+// a=+prompt("a ni kriting")
+// n=+prompt("darajani kriting")
+// for (var i=1;i<=a;i++){
+//     if(n>0){
+//         console.log(i**2);
+//     }
+
+
+// while 
+// N1
+
+// let a = 45;
+// let b = 13;
+
+// while (a > 0) {
+//     if (a > b) {
+//         console.log(a - b, "bosh qolgan joy");
+//         break;
+//     }
+//     a -= b;
+// }
+
+// N2 
+
+// let a=100
+// let b=25
+
+// while (a>0){
+//     if(a>b){
+//         console.log(a/b,"marta joylshgan");
+//         break;
+//     }
+// }
+
+
+// N 3
+
+// let n = 27; 
+
+// while (n > 1) {
+//   if (n % 3 !== 0) {
+//     console.log(n + " soni 3 ning darajasi emas");
+//     break;
+//   }
+//   n = n / 3;
+// }
+
+// if (n === 1) {
+//   console.log("Berilgan son 3 ning darajasi");
+// }
+
+// N 5
+
+// let n = 12;
+// while (n > 0) {
+//     let teskari = n % 10;
+//     console.log(teskari );
+//     n = Math.floor(n / 10);
+//     console.log(teskari,n);
+   
+// }
+
+// N 7
+
+// let n = 32;
+
+// while (n > 1) {
+//   if (n % 2 === 0) {
+//     console.log("2 soni bor");
+//     break;
+//   }
+//   n = n / 3;
+// }
+
+// if (n !== 1) {
+//   console.log("2 soni yo'q");
+// }
+
+// N 10
+
+// let n = 17; 
+
+// let tub_son = true;
+// let i = 2
+
+// while (i < n) {
+//   if (n % i === 0) {
+//     tub_son = false;
+//     break;
+//   }
+//   i++;
+// }
+
+// if (tub_son && n > 1) {
+//   console.log(n + " tub son");
+// } else {
+//   console.log(n + " tub son emas");
+// }
